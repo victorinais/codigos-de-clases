9263cc07dd1ad50955711f67db64c55e2fed1982
victorinais <93883725+victorinais@users.noreply.github.com>
2023-11-17 21:28:36 -0500

    Add files via upload

---
+++ javascript-ejercicios-review/scripts.js
@@ -0,0 +1,54 @@
+/* ejercicio 1 */
+var num1 = 5;
+var num2 = 2;
+
+console.log(num1+num2);
+
+/* ejercicio 2 */
+var simpleString1 = "Hola ";
+var simpleString2 = "mundo";
+
+console.log(simpleString1.length + simpleString2.length);
+
+/* ejercicio 3 */
+var stringUsingString1 = String("JavaScript");
+var stringUsingString2 = String("Ejercicios");
+
+console.log(stringUsingString1, stringUsingString2);
+
+/* ejercicio 4 */
+const stringUsingNewString1 = new String("Concatenar");
+const stringUsingNewString2 = new String("Strings");
+
+console.log(stringUsingNewString1);
+console.log(stringUsingNewString2);
+
+/* ejercicio 5 */
+
+console.log(simpleString1.substring(2,4));
+
+/* ejercicio 6 */
+console.log(simpleString2.includes("ndo"));
+
+/* ejercicio 7 */
+console.log(simpleString1.concat(simpleString2));
+
+/* ejercicio 8 */
+console.log(simpleString1 + simpleString2);
+
+/* ejercicio 9 */
+console.log(`simpleString1` + `simpleString2`);
+
+/* ejercicio 10 */
+var stringWithSpaces1 = " TrimStart";
+var stringWithSpaces2 = "TrimEnd ";
+
+console.log(stringWithSpaces1.trim(),stringWithSpaces2.trim());
+
+/* ejercicio 11 */
+console.log(simpleString1.replace('o','i'));
+
+/* ejercicio 12 */
+console.log(simpleString2.slice(0,3));
+
+/* ejercicio 13 */
