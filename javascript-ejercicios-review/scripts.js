19716101a3f5db3282c5349ff2dccb7225e03b75
victorinais <93883725+victorinais@users.noreply.github.com>
2023-11-20 21:41:48 -0500

    Add files via upload

--- javascript-ejercicios-review/scripts.js
+++ javascript-ejercicios-review/scripts.js
@@ -2,7 +2,7 @@
 var num1 = 5;
 var num2 = 2;
 
-console.log(num1+num2);
+console.log(num1 + num2);
 
 /* ejercicio 2 */
 var simpleString1 = "Hola ";
@@ -25,7 +25,7 @@ console.log(stringUsingNewString2);
 
 /* ejercicio 5 */
 
-console.log(simpleString1.substring(2,4));
+console.log(simpleString1.substring(2, 4));
 
 /* ejercicio 6 */
 console.log(simpleString2.includes("ndo"));
@@ -43,12 +43,108 @@ console.log(`simpleString1` + `simpleString2`);
 var stringWithSpaces1 = " TrimStart";
 var stringWithSpaces2 = "TrimEnd ";
 
-console.log(stringWithSpaces1.trim(),stringWithSpaces2.trim());
+console.log(stringWithSpaces1.trim(), stringWithSpaces2.trim());
 
 /* ejercicio 11 */
-console.log(simpleString1.replace('o','i'));
+console.log(simpleString1.replace('o', 'i'));
 
 /* ejercicio 12 */
-console.log(simpleString2.slice(0,3));
+console.log(simpleString2.slice(0, 3));
 
 /* ejercicio 13 */
+console.log(simpleString2.substring(2,));
+
+/* ejercicio 14 */
+console.log(simpleString1.repeat(3));
+
+/* ejercicio 15 */
+console.log("Esto es una oración de ejemplo".split(" "));
+
+/* ejercicio 16 */
+console.log(simpleString2.toUpperCase());
+
+/* ejercicio 17 */
+console.log(simpleString1.toLowerCase());
+
+/* ejercicio 18 */
+var bool = false;
+console.log(typeof(bool));
+
+/* ejercicio 19 */
+let frutas = ["manzana", "naranja", "banano", "coco"];
+console.log(frutas.length);
+
+/* ejercicio 20 */
+var miCarro = new Object();
+miCarro.marca = "ford";
+miCarro.modelo = 1996;
+miCarro.combustible = "gasolina";
+
+console.log(miCarro.modelo);
+
+/* ejercicio 21 */
+var nulo = null;
+console.log(typeof(nulo));
+
+/* ejercicio 22 */
+var indefinido;
+console.log(typeof(indefinido));
+
+/* ejercicio 23 */
+var decimal = 23.70;
+console.log(typeof(decimal));
+
+/* ejercicio 24 */
+var negativo = -50;
+
+/* ejercicio 25 */
+console.log("casa".indexOf("s"));
+
+/* ejercicio 26 */
+console.log("Javascript".includes("Script"));
+
+/* ejercicio 27 */
+var frase1 = "Hola";
+var frase2 = "Mundo";
+console.log(frase1.concat(frase2));
+
+/* ejercicio 28 */
+var frase1 = "Hola";
+var frase2 = "Mundo";
+console.log(frase1+frase2);
+
+/* ejercicio 29 */
+console.log(`Hola`+`Mundillo`);
+
+/* ejercicio 30 */
+var cadenaConEspacios = " Ejemplo con espacios ";
+console.log(cadenaConEspacios.trimStart(),cadenaConEspacios.trimEnd());
+
+/* ejercicio 31 */
+console.log("El cielo es azul".replace("azul", "rojo"));
+
+/* ejercicio 32 */
+console.log("Programación".slice(-4));
+
+/* ejercicio 33 */
+console.log("JavaScript".substring(2));
+
+/* ejercicio 34 */
+console.log("Hola ".repeat(3));
+
+/* ejercicio 35 */
+console.log("Esto es una oración de ejemplo".split(" "));
+
+/* ejercicio 36 */
+console.log("Javascript".toUpperCase());
+
+/* ejercicio 37 */
+console.log("EJEMPLO".toLowerCase());
+
+/* ejercicio 38 */
+var numero = 7;
+console.log(typeof(numero));
+
+/* ejercicio 39 */
+var bo = true;
+console.log(typeof(bo));
