3177bca569f131cf8011292fa5839751d8062a77
victorinais <93883725+victorinais@users.noreply.github.com>
2023-11-22 21:29:37 -0500

    Add files via upload

---
+++ Ejercicios con Condicionales en JavaScript/script.js
@@ -0,0 +1,111 @@
+/* ejercicio 1 */
+var num = 5;
+if (num > 0){
+    console.log("el numero es positivo");
+}else {
+    console.log("el numero es negativo");
+}
+
+
+/* ejercicio 2 */
+var num = -5;
+if (num < 0){
+    console.log("el numero es negativo");
+}else {
+    console.log("el numero es positivo");
+}
+
+/* ejercicio 3 */
+var num = 6;
+if (num % 2 == 0){
+        console.log("el numero es par");
+}else {
+    console.log("el numero es impar");
+}
+
+/* ejercicio 4 */
+var num = 7;
+if (num % 2 == 0){
+        console.log("el numero es par");
+}else {
+    console.log("el numero es impar");
+}
+
+/* ejercicio 5 */
+var num = -2;
+if (num * 5 > 5) {
+    console.log("el numero es multiplo de 5");
+}else {
+    console.log("el numero no es multiplo de 5");
+}
+
+/* ejercicio 6 */
+var num = 7;
+if (num % 3 == 0) {
+    console.log("el numero es divisible entre 3");
+}else {
+    console.log("el numero no es divisible entre 3");
+}
+
+/* ejercicio 7 */
+var num = 10;
+if (num > 100) {
+    console.log("el numero es mayor a 100");
+}else {
+    console.log("el numero es menor a 100");
+}
+
+/* ejercicio 8 */
+var num = -42;
+if (num < -50) {
+ console.log("el numero es menor a -50");
+}else {
+    console.log("el numero es mayor a -50");
+}
+
+/* ejercicio 9 */
+var num = 20;
+if (num >= 20 && num <= 50) {
+    console.log("el numero está entre 20 y 50");
+}else {
+    console.log("el numero no está entre 20 y 50");
+}
+
+/* ejercicio 10 */
+var num = 0;
+if (num == 0) {
+    console.log("el numero es igual a cero");
+}else {
+    console.log("el numero es diferentre a cero");
+}
+
+/* ejercicio 11 */
+var num = -11;
+if (num > -10 & num < 10) {
+    console.log("el numero es mayor a -10 y menor a 10");
+}else {
+    console.log("el numero es menor a -10 o mayor a 10");
+}
+
+/* ejercicio 12 */
+var num = 2005;
+if (num % 4 == 0 & num % 100 > 0) {
+    console.log("el año es biciesto");
+}else {
+    console.log("el año no es biciesto");
+}
+
+/* ejercicio 13 */
+var num = 18;
+if (num >= 18) {
+    console.log("la persona es mayor de edad");
+}else {
+    console.log("la persona es menor de edad");
+}
+
+/* ejercicio 15 */
+var num = 5;
+
+
+
+
