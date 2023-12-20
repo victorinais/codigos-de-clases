64f578c11ea8d1cf6de37f2f2b37d1ea55703750
victorinais <93883725+victorinais@users.noreply.github.com>
2023-12-20 17:33:50 -0500

    Add files via upload

--- proyecto instagram/script-home.js
+++ proyecto instagram/script-home.js
@@ -1,3 +1,5 @@
+
+
 let btn = document.getElementById("btn");
 btn.addEventListener("click", cerrarSesion);
 
@@ -7,7 +9,7 @@ let genero = sessionStorage.getItem("genero");
 
 
 document.getElementById('nombre').innerText = nombre;
-document.getElementById('ver').innerText = genero;
+
 
 
 if (auth != 1) {
@@ -18,14 +20,89 @@ function cerrarSesion() {
     sessionStorage.setItem("nombre", "");
     sessionStorage.setItem("auth", "");
     sessionStorage.setItem("genero", "");
+    sessionStorage.setItem("pos", "")
     location.href = "index.html";
 }
 
 
-if (genero == "femenino"){
+if (genero == "femenino") {
     document.getElementById("body").classList.remove("Masculino");
     document.getElementById("body").classList.add("Femenino");
 } else {
     document.getElementById("body").classList.remove("Femenino");
     document.getElementById("body").classList.add("Masculino");
 }
+
+let row = document.getElementById('row');
+
+let persons = [
+
+    {
+        nombre: "Samuel",
+        correo: "samuel@gmail.com",
+        genero: "masculino",
+        password: "password",
+        images: [
+            './images/img-11.jpg',
+            './images/img-12.jpg',
+            './images/img-13.jpeg',
+            './images/img-14.webp',
+            './images/img-15.jpg',
+            './images/img-16.jpg',
+            './images/img-17.webp',
+            './images/img-18.jpg',
+            './images/img-19.jpg',
+            './images/img-20.jpg',
+        ]
+    }, {
+        nombre: "pedro",
+        correo: "pedro@gmail.com",
+        genero: "masculino",
+        password: "password",
+        images: [
+            './images/img-21.jpg',
+            './images/img-22.jpg',
+            './images/img-23.jpg',
+            './images/img-24.jpg',
+            './images/img-25.jpg',
+            './images/img-26.jpg',
+            './images/img-27.jpg',
+            './images/img-28.jpg',
+            './images/img-29.jpg',
+            './images/img-30.jpg',
+        ]
+    }, {
+        nombre: "Manuela",
+        correo: "manuela@gmail.com",
+        genero: "femenino",
+        password: "password",
+        images: [
+            './images/img-1.jpg',
+            './images/img-2.jpeg',
+            './images/img-3.jpg',
+            './images/img-4.jpg',
+            './images/img-5.webp',
+            './images/img-6.jpg',
+            './images/img-7.jpg',
+            './images/img-8.jpg',
+            './images/img-9.webp',
+            './images/img-10.jpeg',
+        ]
+    }
+]; 
+
+let pos = sessionStorage.getItem("pos");
+
+persons[pos].images.forEach(function (image) {
+    let col = document.createElement('div');
+    col.classList.add("col-md-3");
+    row.appendChild(col);
+
+    // img
+    let img = document.createElement('img');
+    img.setAttribute('src', image)
+    img.style.width = "300px";
+    img.style.height = "100%";
+    col.appendChild(img);
+
+});
--- proyecto instagram/script-index.js
+++ proyecto instagram/script-index.js
@@ -29,16 +29,16 @@ function iniciarSesion() {
             genero: "masculino",
             password: "password",
             images: [
-                './images/img-11.jpg',
-                './images/img-12.jpg',
-                './images/img-13.jpeg',
-                './images/img-14.webp',
-                './images/img-15.jpg',
-                './images/img-16.jpg',
-                './images/img-17.webp',
-                './images/img-18.jpg',
-                './images/img-19.jpg',
-                './images/img-20.jpg',
+                './images/img-21.jpg',
+                './images/img-22.jpg',
+                './images/img-23.jpg',
+                './images/img-24.jpg',
+                './images/img-25.jpg',
+                './images/img-26.jpg',
+                './images/img-27.jpg',
+                './images/img-28.jpg',
+                './images/img-29.jpg',
+                './images/img-30.jpg',
             ]
         }, {
             nombre: "Manuela",
@@ -65,16 +65,19 @@ function iniciarSesion() {
     let password = document.getElementById("Contraseña").value;
 
 
-    persons.forEach(function (person) {
+    persons.forEach(function (person, key) {
         if (correo == person.correo && password == person.password) {
             sessionStorage.setItem("nombre", person.nombre);
             sessionStorage.setItem("genero", person.genero);
+            sessionStorage.setItem("pos", key)
             sessionStorage.setItem("auth", 1);
             location.href = "home.html";
         }
     });
 
     alert("Los datos no coinciden");
+
+
 
 
 }
